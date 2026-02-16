30435ae859faf6962f2bd26514edb753c3b4244c
Pedro Alves <pedronerydev@gmail.com>
2026-02-16 15:40:37 -0300

    feat: gerando repository de coupon

---
+++ src/main/java/com/pedro/dev/couponservice/repository/CouponRepository.java
@@ -0,0 +1,15 @@
+package com.pedro.dev.couponservice.repository;
+
+import com.pedro.dev.couponservice.domain.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+
+    @Query(value = "SELECT count(*) > 0 FROM coupons WHERE id = :id AND deleted = true", nativeQuery = true)
+    boolean isAlreadyDeleted(UUID id);
+}
